0d395654471931c4eebd5b76b44ec1a8e9021db0
FedorSvetlichnyy <svfedor84@gmail.com>
2023-01-16 20:22:07 +0300

    1 Arithmetic

--- src/main/java/Part1/ObjectsAndClasses/Arithmetic/Arithmetic.java
+++ src/main/java/Part1/ObjectsAndClasses/Arithmetic/Arithmetic.java
@@ -1,4 +1,24 @@
-package Part1.ObjectsAndClasses.Arithmetic;
+package main.java.Part1.ObjectsAndClasses.Arithmetic;
 
 public class Arithmetic {
+    private int a;
+    private int b;
+
+    public Arithmetic(int a, int b) {
+        this.a = a;
+        this.b = b;
+    }
+    public int Sum() {
+        return a + b;
+    }
+    public int Multiplication() {
+        return a * b;
+    }
+    public int Min() {
+        return (a <= b) ? a : b;
+    }
+    public int Max() {
+        return (a >= b) ? a : b;
+    }
+
 }
--- src/main/java/Part1/ObjectsAndClasses/Arithmetic/Main.java
+++ src/main/java/Part1/ObjectsAndClasses/Arithmetic/Main.java
@@ -1,7 +1,14 @@
-package Part1.ObjectsAndClasses.Arithmetic;
+package main.java.Part1.ObjectsAndClasses.Arithmetic;
 
 public class Main {
     public static void main(String[] args) {
+        Arithmetic arithmetic = new Arithmetic(3,12);
+
+        System.out.println("Сумма чисел равна: " + arithmetic.Sum());
+        System.out.println("Произведение чисел равно: " + arithmetic.Multiplication());
+        System.out.println("Максимальное из двух чисел: " + arithmetic.Max());
+        System.out.println("Миниимальное из двух чисел: " + arithmetic.Min());
+
 
     }
 }

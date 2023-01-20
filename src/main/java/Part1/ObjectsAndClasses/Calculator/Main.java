7674879af5746d91ae2931c721db0a7cc8f304db
FedorSvetlichnyy <svfedor84@gmail.com>
2023-01-20 20:28:43 +0300

    сalculator

--- src/main/java/Part1/ObjectsAndClasses/Calculator/Main.java
+++ src/main/java/Part1/ObjectsAndClasses/Calculator/Main.java
@@ -1,7 +1,14 @@
-package Part1.ObjectsAndClasses.Calculator;
+package main.java.Part1.ObjectsAndClasses.Calculator;
+
+import static main.java.Part1.ObjectsAndClasses.Calculator.Operation.*;
 
 public class Main {
     public static void main(String[] args) {
+        Calculator calculator = new Calculator(5, 5);
+
+        calculator.calculate(ADD);
+        calculator.calculate(SUBTRACT);
+        calculator.calculate(MULTIPLY);
 
     }
 }

5798e67d54ff8e71660c98540c58ed80009debd6
FedorSvetlichnyy <svfedor84@gmail.com>
2023-01-30 20:37:20 +0300

    compuhter

--- src/main/java/Part1/ObjectsAndClasses/Computer/Main.java
+++ src/main/java/Part1/ObjectsAndClasses/Computer/Main.java
@@ -1,7 +1,14 @@
-package Part1.ObjectsAndClasses.Computer;
+package main.java.Part1.ObjectsAndClasses.Computer;
 
 public class Main {
     public static void main(String[] args) {
+        Computer computer = new Computer("Russia", "PK Home",
+                new CPU(1.9, 4, "Intel", 10),
+                new RAM("DDR4", 6, 20),
+                new HDD("SSD", 120, 80),
+                new Display(21.1, "VA", 100),
+                new Keyboard("mechanical", false, 20));
+        System.out.println(computer);
 
     }
 }

f375256594ac26a795c28da2c275f665df66693c
sergey.burlakov <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>
2023-04-27 15:00:42 +0300

    added part2

---
+++ src/main/java/Part2/ReverseArray/ReverseArray.java
@@ -0,0 +1,10 @@
+package Part2.ReverseArray;
+
+public class ReverseArray {
+
+    public static String[] reverse(String[] strings) {
+        //TODO: Напишите код, который меняет порядок расположения элементов внутри массива на обратный.
+        return strings;
+    }
+
+}

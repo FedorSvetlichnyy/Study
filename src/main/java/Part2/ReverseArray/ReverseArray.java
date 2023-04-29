7975379e9394f6c309b9dac0b31a3aa5c702e50f
FedorSvetlichnyy <svfedor84@gmail.com>
2023-04-29 20:28:27 +0300

    ReverseArray

--- src/main/java/Part2/ReverseArray/ReverseArray.java
+++ src/main/java/Part2/ReverseArray/ReverseArray.java
@@ -1,9 +1,19 @@
 package Part2.ReverseArray;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.Comparator;
+
 public class ReverseArray {
 
     public static String[] reverse(String[] strings) {
         //TODO: Напишите код, который меняет порядок расположения элементов внутри массива на обратный.
+        String temp = "";
+        for (int i = 0; i < strings.length / 2; i++) {
+            temp = strings[i];
+            strings[i] = strings[strings.length - 1 - i];
+            strings[strings.length - 1 - i] = temp;
+        }
         return strings;
     }
 

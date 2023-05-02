20260630421c0df35c8cb069335a7920671a0d51
FedorSvetlichnyy <svfedor84@gmail.com>
2023-05-02 22:00:31 +0300

    Printer

--- src/main/java/Part2/Printer/Main.java
+++ src/main/java/Part2/Printer/Main.java
@@ -1,7 +1,26 @@
 package Part2.Printer;
 
+import java.util.Arrays;
+import java.util.LinkedList;
+import java.util.Queue;
+
 public class Main {
     public static void main(String[] args) {
+        Queue<Document> print = new LinkedList<>();
+        Printer printer = new Printer(print);
+        Document document = new Document(2);
+        Document document2 = new Document(6);
+
+        printer.append(print, document);
+        printer.append(print, document2);
+        System.out.println(printer.getPendingPagesCount(print));
+        System.out.println(printer.getAllPagesCount(print));
+        //printer.clear(print);
+        printer.print(print);
+        System.out.println(printer.getPendingPagesCount(print));
+        System.out.println(printer.getAllPagesCount(print));
+
+
 
     }
 }
--- src/main/java/Part2/Printer/Printer.java
+++ src/main/java/Part2/Printer/Printer.java
@@ -1,4 +1,53 @@
 package Part2.Printer;
 
+import java.lang.reflect.Array;
+import java.util.*;
+
 public class Printer {
+
+    Queue<Document> print = new LinkedList<>();
+    static int countPages;
+    static int allPrintPages;
+
+    public Printer(Queue<Document> print) {
+        this.print = print;
+    }
+
+    public void append(Queue<Document> print, Document b) {
+        countPages += b.getCountPages();
+        allPrintPages += b.getCountPages();
+        print.add(b);
+    }
+
+    public void clear(Queue<Document> print) {
+        print.clear();
+        countPages = 0;
+    }
+
+    public void print(Queue<Document> print) {
+        System.out.println(Arrays.toString(print.toArray()));
+        print.clear();
+        countPages = 0;
+    }
+
+    public int getPendingPagesCount(Queue<Document> print) {
+        return countPages;
+    }
+
+    public int getAllPagesCount(Queue<Document> print) {
+        return allPrintPages;
+    }
+
+}
+
+class Document {
+    int countPages;
+
+    public Document(int countPages) {
+        this.countPages = countPages;
+    }
+
+    public int getCountPages() {
+        return countPages;
+    }
 }

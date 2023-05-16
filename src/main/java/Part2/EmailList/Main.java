1408d8f491d925076d975debafa29c89c3f2741b
FedorSvetlichnyy <svfedor84@gmail.com>
2023-05-16 13:32:21 +0300

    EmailList

--- src/main/java/Part2/EmailList/Main.java
+++ src/main/java/Part2/EmailList/Main.java
@@ -1,9 +1,14 @@
 package Part2.EmailList;
 
+import java.sql.SQLOutput;
 import java.util.Scanner;
+import java.util.TreeSet;
 
 public class Main {
     public static final String WRONG_EMAIL_ANSWER = "Неверный формат email";
+
+    static EmailList emailList = new EmailList();
+
     
     /* TODO:
         Пример вывода списка Email, после ввода команды LIST в консоль:
@@ -20,16 +25,41 @@ public class Main {
     */
 
     public static void main(String[] args) {
+        System.out.println("Программа для хранения адресов электронной почты. " +
+                "Адреса можно добавлять командой ADD и печатать весь список командой LIST. 0 - выход.");
         Scanner scanner = new Scanner(System.in);
-        
+
         while (true) {
             String input = scanner.nextLine();
             if (input.equals("0")) {
                 break;
             }
-            
+
             //TODO: write code here
-            
+            int value = input.indexOf(' ');
+            String menu;
+            String email;
+            try {
+                menu = input.substring(0, value);
+            } catch (StringIndexOutOfBoundsException e) {
+                menu = input;
+            }
+            try {
+                email = input.substring(value + 1);
+            } catch (StringIndexOutOfBoundsException e) {
+                email = null;
+            }
+            switch (menu) {
+                case "LIST":
+                    emailList.printSortedEmails();
+                    break;
+                case "ADD":
+                    emailList.add(email);
+                    break;
+                default:
+                    System.out.println("Что-то не так, попробуйте еще раз.");
+                    break;
+            }
         }
     }
 }

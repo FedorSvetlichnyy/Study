b443b101aa424d81052c4f3776359623413e15dd
Fedor Svetlichnyy <svfedor84@gmail.com>
2025-06-14 17:13:06 +0300

    StringToDigit

--- src/main/java/Part1/DigitsAndStrings/StringToDigit.java
+++ src/main/java/Part1/DigitsAndStrings/StringToDigit.java
@@ -1,24 +1,27 @@
-package main.java.Part1.DigitsAndStrings;
+package Part1.DigitsAndStrings;
 
 import static java.lang.Integer.parseInt;
 
 public class StringToDigit {
     public static void main(String[] args) {
         String str1 = "12345";
-        String str2 = "10";
+        String str2 = "11";
         String str3 = "25059191";
-        sumDigits(str1);
-        sumDigits(str2);
-        sumDigits(str3);
+        System.out.println(sumDigits(str1));
+        System.out.println(sumDigits(str2));
+        System.out.println(sumDigits(str3));
 
     }
-    public static void sumDigits(String str){
+    public static Integer sumDigits(String str){
+        if (str == null) {
+            return 0;
+        }
         int x = parseInt(str);
         int sum = 0;
         while (x > 0) {
             sum = sum + x % 10;
             x = x / 10;
         }
-        System.out.println(sum);
+        return sum;
     }
 }
